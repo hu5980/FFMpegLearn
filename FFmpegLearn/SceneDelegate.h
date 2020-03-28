84cd21de1e43b5eeb2f281a4722d19c7d229ab24
胡光耀 <hugy@500wan.com>
2020-03-29 00:16:32 +0800

    修改工程名

---
+++ FFmpegLearn/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  FFMpegLearn
+//
+//  Created by gavin hu on 2020/3/29.
+//  Copyright © 2020 gavin hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
